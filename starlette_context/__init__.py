e5bff0660b6c2fe4986171dd3a81cd5bfc6716e0
Tom Wojcik <tomasz.wooojcik@gmail.com>
2019-12-28 13:16:17 +0100

    add rid and cid to response headers if using middleware that handles them

--- starlette_context/__init__.py
+++ starlette_context/__init__.py
@@ -58,30 +58,38 @@ class CreateEmptyContextMiddleware(BaseHTTPMiddleware):
         return response
 
 
-class PreserveCustomContextMiddleware(CreateEmptyContextMiddleware):
+class PreserveCustomContextMiddleware(BaseHTTPMiddleware):
     cid = "X-Correlation-ID"
     rid = "X-Request-ID"
     date = "Date"
     forwarded_for = "X-Forwarded-For"
     ua = "User-Agent"
 
-    @staticmethod
-    def get_uuid() -> str:
-        return uuid4().hex
+    def __init__(self, *args, **kwargs):
+        super(PreserveCustomContextMiddleware, self).__init__(*args, **kwargs)
+        self.cid_value = None
+        self.rid_value = None
 
     @staticmethod
     def get_from_header_by_key(request: Request, key: str):
+        # http/2 headers lowercase
+        if key != key.lower():
+            return request.headers.get(key) or request.headers.get(key.lower())
         return request.headers.get(key)
 
     def get_request_id(self, request: Request) -> str:
-        return (
-            self.get_from_header_by_key(request, self.rid) or self.get_uuid()
-        )
+        if not self.rid_value:
+            self.rid_value = (
+                self.get_from_header_by_key(request, self.rid) or uuid4().hex
+            )
+        return self.rid_value
 
     def get_correlation_id(self, request: Request) -> str:
-        return (
-            self.get_from_header_by_key(request, self.cid) or self.get_uuid()
-        )
+        if not self.cid_value:
+            self.cid_value = (
+                self.get_from_header_by_key(request, self.cid) or uuid4().hex
+            )
+        return self.cid_value
 
     def get_user_agent(self, request: Request) -> str:
         return self.get_from_header_by_key(request, self.ua) or None
@@ -100,3 +108,17 @@ class PreserveCustomContextMiddleware(CreateEmptyContextMiddleware):
             self.forwarded_for: self.get_forwarded_for(request),
             self.ua: self.get_user_agent(request),
         }
+
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ):
+        token: Token = _request_scope_context_storage.set(
+            self.set_context(request)
+        )  # noqa
+        try:
+            response = await call_next(request)
+            response.headers[self.cid] = self.cid_value
+            response.headers[self.rid] = self.rid_value
+        finally:
+            _request_scope_context_storage.reset(token)
+        return response
