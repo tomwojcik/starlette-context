62dca2a80530f21cc70c098d18ce3725e0d42495
gino <gino.mempin@gmail.com>
2021-04-11 13:16:38 +0900

    Revert stub files added from https://github.com/tomwojcik/starlette-context/pull/34

--- starlette_context/__init__.pyi
+++
@@ -1,1 +0,0 @@
-from starlette_context.ctx import context as context
--- starlette_context/plugins/request_id.pyi
+++
@@ -1,4 +0,0 @@
-from starlette_context.plugins.base import PluginUUIDBase
-
-class RequestIdPlugin(PluginUUIDBase):
-    key: str
