6638504c4e702578fa23ae4b163a65ffc79d798a
Tom Wojcik <me@tomwojcik.com>
2021-06-28 16:41:47 +0200

    fix broken packaging in github action

--- setup.py
+++ setup.py
@@ -2,7 +2,26 @@ from distutils.core import setup
 
 import setuptools
 
-from starlette_context import __author__, __version__  # noqa
+import os
+import re
+
+
+PKG_NAME = "starlette_context"
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+PATTERN = r'^{target}\s*=\s*([\'"])(.+)\1$'
+
+AUTHOR_RE = re.compile(PATTERN.format(target="__author__"), re.M)
+VERSION_RE = re.compile(PATTERN.format(target="__version__"), re.M)
+
+
+def parse_init():
+    with open(os.path.join(HERE, PKG_NAME, "__init__.py")) as f:
+        file_data = f.read()
+    return [
+        regex.search(file_data).group(2) for regex in (AUTHOR_RE, VERSION_RE)
+    ]
 
 
 def get_long_description():
@@ -10,25 +29,26 @@ def get_long_description():
         return f.read()
 
 
+AUTHOR, VERSION = parse_init()
+
+
 setup(
-    name="starlette_context",
+    name=PKG_NAME,
     python_requires=">=3.7",
-    version=__version__,
+    version=VERSION,
     license="MIT",
     description="Access context in Starlette",
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(
-        include=("starlette_context", "starlette_context.*")
-    ),
+    packages=setuptools.find_packages(include=(PKG_NAME, f"{PKG_NAME}.*")),
     package_data={
-        "starlette_context": ["py.typed"],
+        PKG_NAME: ["py.typed"],
     },
     platforms="any",
-    author=__author__,
+    author=AUTHOR,
     url="https://github.com/tomwojcik/starlette-context",
     download_url="https://github.com/tomwojcik/starlette-context/"
-    f"archive/{__version__}.tar.gz",
+    f"archive/{VERSION}.tar.gz",
     keywords=["starlette", "fastapi"],
     install_requires="starlette",
     classifiers=[
