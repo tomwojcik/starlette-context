3415fc326aaa5fbccd8181e6c784bc0d5785a6e7
Tomasz Wójcik <tomwojcik@users.noreply.github.com>
2022-11-26 23:23:12 +0100

    Simple test case to check if header is available in failed request response, if handled (#79)
    * add test case for header from plugin on fail

--- tests/test_context/test_context_on_unhandled_exception.py
+++ tests/test_context/test_context_on_unhandled_exception.py
@@ -5,21 +5,37 @@ from starlette.requests import Request
 from starlette.responses import Response
 from starlette.testclient import TestClient
 
-from starlette_context import middleware, plugins
+from starlette_context import middleware, plugins, context
 from starlette_context.header_keys import HeaderKeys
+
+
+class CustomExc(Exception):
+    pass
+
+
+def custom_exc_handler(request: Request, exc):
+    return Response(status_code=400, headers=context.data)
+
 
 app = Starlette(
     middleware=[
         Middleware(
-            middleware.ContextMiddleware, plugins=(plugins.RequestIdPlugin(),)
+            middleware.RawContextMiddleware,
+            plugins=(plugins.RequestIdPlugin(),),
         )
-    ]
+    ],
+    exception_handlers={CustomExc: custom_exc_handler},
 )
 
 
 @app.route("/exc")
 async def exc(request: Request):
     raise Exception
+
+
+@app.route("/handled")
+async def handled(request: Request):
+    raise CustomExc
 
 
 @app.route("/")
@@ -38,3 +54,7 @@ def test_context_persistence():
     resp = client.get("/exc")
     assert resp.status_code == 500
     assert HeaderKeys.request_id.lower() not in resp.headers
+
+    resp = client.get("/handled")
+    assert resp.status_code == 400
+    assert HeaderKeys.request_id.lower() in resp.headers
