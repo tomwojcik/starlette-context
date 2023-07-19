1a4da45f56e141dfc90ca490ad719a801acea74e
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2023-07-19 21:45:45 +0400

    Fix black formatting for plugins

--- starlette_context/plugins/__init__.py
+++ starlette_context/plugins/__init__.py
@@ -1,9 +1,17 @@
-from .api_key import ApiKeyPlugin
-from .base import Plugin
-from .correlation_id import CorrelationIdPlugin
-from .date_header import DateHeaderPlugin
-from .forwarded_for import ForwardedForPlugin
-from .request_id import RequestIdPlugin
 from .user_agent import UserAgentPlugin
+from .request_id import RequestIdPlugin
+from .forwarded_for import ForwardedForPlugin
+from .date_header import DateHeaderPlugin
+from .correlation_id import CorrelationIdPlugin
+from .base import Plugin
+from .api_key import ApiKeyPlugin
 
-__all__ = ["ApiKeyPlugin", "Plugin", "CorrelationIdPlugin", "DateHeaderPlugin", "ForwardedForPlugin", "RequestIdPlugin", "UserAgentPlugin"]
+__all__ = [
+    "ApiKeyPlugin",
+    "Plugin",
+    "CorrelationIdPlugin",
+    "DateHeaderPlugin",
+    "ForwardedForPlugin",
+    "RequestIdPlugin",
+    "UserAgentPlugin",
+]
